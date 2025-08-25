769edcf6bfd03bd6e7d16761f55e46e6e61ab2c6
CrazyLeiGe <140936390+CrazyLeiGe@users.noreply.github.com>
2025-08-25 23:32:53 +0800

    调整博客设置

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export const SITE = {
   website: "https://www.d1lab.com/", // 用你已部署的域名替换此处内容
   author: "疯狂的石头",
   profile: "null",
-  desc: "一个在互联网上冲浪者的个人博客.",
+  desc: "一个互联网上的冲浪者，有一颗爱好折腾的心。",
   title: "D1实验室",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
@@ -18,6 +18,6 @@ export const SITE = {
   },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
-  lang: "en", // HTML语言代码。将其留空，默认值将为“en”
+  lang: "zh-CN", // HTML语言代码。将其留空，默认值将为“en”
   timezone: "Asia/Shanghai", // 默认的全局时区（IANA格式） https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
